d782fe5fce4f84cbb7de44b867e2646f6243c5b3
debruyne1996 <badebruyne@oakland.edu>
2019-01-14 20:24:32 +0000

    addded weather

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+ <head>
+  <link rel="stylesheet" type="text/css" href="assets/css/index.css">
+<script src="assets/js/clock.js"></script> 
+<script src="assets/js/date.js"></script> 
+<script src="assets/js/calendar.js"></script>
+<script src="assets/js/index.js"></script>
+</head> 
+<html>
+  <body>
+    <div id="MyClockDisplay" class="clock">
+    <script>
+      showTime();
+    </script>
+    </div>
+    <div id="MyDateDisplay" class="date">
+    <script>
+      showDate()
+      </script>
+    </div>
+    <div class="weather">
+<a class="weatherwidget-io" href="https://forecast7.com/en/42d68n83d13/rochester/?unit=us" data-label_1="ROCHESTER" data-label_2="WEATHER" data-basecolor="#000000" data-accent="#000000" data-textcolor="#ffffff" data-highcolor="#ffffff" data-lowcolor="#ffffff" data-suncolor="#f4ff00" data-mooncolor="#cccccc" data-cloudcolor="#cccccc" data-cloudfill="#cccccc" data-raincolor="#cccccc" data-snowcolor="#cccccc" >ROCHESTER WEATHER</a>
+<script>
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+</script>
+    </div>
+  </body>
+</html>
+
